0f03a2d90126004185120768da4fc6218496ec95
Martin Jordán <martin@jordan.cz>
2018-07-26 14:32:42 +0200

    refactoring

--- src/components/common/Card.js
+++ src/components/common/Card.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { View } from 'react-native';
 
-const Card = (props) => (
+const Card = (props) => {
+  return(
     <View style={styles.containerStyle}>
       {props.children}
     </View>
   );
+};
 
 const styles = {
   containerStyle: {
--- src/components/common/CardSection.js
+++ src/components/common/CardSection.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { View } from 'react-native';
 
-const CardSection = (props) => (
+const CardSection = (props) => {
+  return (
     <View style={styles.containerStyle}>
       {props.children}
     </View>
   );
+};
+
 
 const styles = {
   containerStyle: {
